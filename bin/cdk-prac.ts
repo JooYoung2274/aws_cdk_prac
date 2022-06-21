626ad75b89fe9e55f46ef8ef151157c0e9bc2e66
JooYoung Kim <jooooo2274@gmail.com>
2022-06-21 16:45:07 +0900

    Delete cdk-prac.ts

--- bin/cdk-prac.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import "source-map-support/register";
-import * as cdk from "aws-cdk-lib";
-import { CdkPracStack } from "../lib/cdk-prac-stack";
-
-const app = new cdk.App();
-new CdkPracStack(app, "CdkPracStack", {});
