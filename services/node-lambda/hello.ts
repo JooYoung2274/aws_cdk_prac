671768839b887112f8a3499ef4331598e97c1e7d
JooYoung Kim <jooooo2274@gmail.com>
2022-06-22 18:00:34 +0900

    [update] 디버깅을 위한 세팅

--- services/node-lambda/hello.ts
+++ services/node-lambda/hello.ts
@@ -1,9 +1,14 @@
-import { v4 } from "uuid";
+import { S3 } from "aws-sdk";
+
+const s3Client = new S3();
 
 async function handler(event: any, context: any) {
+  const buckets = await s3Client.listBuckets().promise();
+
+  console.log(event);
   return {
     statusCode: 200,
-    body: "Hello from lambda" + v4,
+    body: "Here are your buckets" + JSON.stringify(buckets.Buckets),
   };
 }
 
@@ -11,3 +16,4 @@ export { handler };
 
 // 디펜던시가 포함된 Lambda-nodejs 코드
 // npm install --save-dev esbuild@0 해당 라이브러리를 통해 사용 가능함.
+// 웹팩설정보다 훨씬 편함
