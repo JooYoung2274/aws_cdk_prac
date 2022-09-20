05d29632ef57b59d34b3920bebf3740c90f0711b
JooYoung2274 <jooooo2274@gmail.com>
2022-09-20 09:58:35 +0900

    [update] new setting

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { CdkStack } from '../lib/cdk-stack';
+import { SpaceStack } from '../lib/cdk-stack';
 
 const app = new cdk.App();
-new CdkStack(app, 'CdkStack');
+new SpaceStack(app, 'Space-finder', {
+    stackName: 'SpaceFinder',
+});
--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -1,54 +1,8 @@
-import { Duration, Stack, StackProps } from "aws-cdk-lib";
-import * as sns from "aws-cdk-lib/aws-sns";
-import * as subs from "aws-cdk-lib/aws-sns-subscriptions";
-import * as sqs from "aws-cdk-lib/aws-sqs";
-import { Construct } from "constructs";
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
 
-import {
-  Code,
-  Function as LambdaFunction,
-  Runtime,
-} from "aws-cdk-lib/aws-lambda";
-
-import { LambdaIntegration, RestApi } from "aws-cdk-lib/aws-apigateway";
-
-import { join } from "path";
-import { GenericTable } from "./GenericTable";
-
-import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
-import { PolicyStatement } from "aws-cdk-lib/aws-iam";
-
-export class CdkStack extends Stack {
-  private api = new RestApi(this, "SpaceApi");
-
-  //dynamodb class를 따로 만들어서 (genericTable.ts) 가져와서 생성.
-  private spaceTable = new GenericTable("SpaceTable", "spaceId", this);
-
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
-
-    // 일반 Lambda 코드
-    const helloLambda = new LambdaFunction(this, "helloLambda", {
-      runtime: Runtime.NODEJS_16_X,
-      code: Code.fromAsset(join(__dirname, "..", "services", "hello")),
-      handler: "hello.main",
-    });
-
-    // 디펜던시가 포함된 Lambda-nodejs 코드
-    const helloLambdaNodeJs = new NodejsFunction(this, "helloLambdaNodeJs", {
-      entry: join(__dirname, "..", "services", "node-lambda", "hello.ts"),
-      handler: "handler",
-    });
-
-    // s3에 대한 권한 부여
-    // * 와일드카드 쓰지마셈
-    const s3ListPolicy = new PolicyStatement();
-    s3ListPolicy.addActions("s3:ListAllMyBuckets");
-    s3ListPolicy.addResources("*");
-    helloLambdaNodeJs.addToRolePolicy(s3ListPolicy);
-
-    const helloLambdaIntegration = new LambdaIntegration(helloLambda);
-    const helloLambdaResource = this.api.root.addResource("hello");
-    helloLambdaResource.addMethod("GET", helloLambdaIntegration);
-  }
+export class SpaceStack extends Stack {
+    constructor(scope: Construct, id: string, props: StackProps) {
+        super(scope, id, props);
+    }
 }
